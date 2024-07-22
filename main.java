05c08642d4b6af885390e8bb564a540fdb4579f8
rajatjuyal <rajat.juyal@gmail.com>
2024-07-23 02:08:47 +0530

    new print to commut

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main{
 
     public static void main(String[] args) {
         System.out.println("hello this is some program");
+        System.out.println("new print statment");
     }
 }
